c63a124f1b7988a6d389a08f5ee494c58dcb43c6
shani-segev-lmnd <shani.segev@lemonade.com>
2025-12-29 15:01:22 +0200

    Fix get_relation to filter by resource_type=model to avoid finding analyses with same name

--- macros/database_specific_helpers/get_relation.sql
+++ macros/database_specific_helpers/get_relation.sql
@@ -1,6 +1,6 @@
 {% macro get_relation(relation_name) %}
     {% if execute %}
-        {% set model_get_relation_node = graph.nodes.values() | selectattr('name', 'equalto', relation_name) | first %}
+        {% set model_get_relation_node = graph.nodes.values() | selectattr('resource_type', 'equalto', 'model') | selectattr('name', 'equalto', relation_name) | first %}
         {% set relation = api.Relation.create(
             database = model_get_relation_node.database,
             schema = model_get_relation_node.schema,
